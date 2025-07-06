60c9057d01256d95941cd64355ac61f55993d256
DM-kun <86197998+DMMaster636@users.noreply.github.com>
2025-07-06 03:22:28 +0200

    credits

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 -- name: [CS] MARIO '96
--- description: The OFFICIAL \\#8B0000\\MARIO '85\\#FFFFFF\\ in \\#FF0000\\S\\#007FFF\\M\\#00FF00\\6\\#FFFF00\\4\\#FFFFFF\\ Character Select Pack!\n\nCommands:\n/hud96 [on|off] - toggle the custom hud\n\nCredits:\nModels by \\#007FFF\\5UP34\\#FFFFFF\\\nCode by \\#CA8DFE\\DM-kun\\#FFFFFF\\ and \\#06402B\\Squishy\\#FFFFFF\\\nVoice Acting By Sunset + The Hall Monitor\nCharacters Owned by RazzDazzleDoo\n\\#ffffff\\\n\nThis Pack requires Character Select to use as a Library!
+-- description: The OFFICIAL \\#8B0000\\MARIO '85\\#FFFFFF\\ in \\#FF0000\\S\\#007FFF\\M\\#00FF00\\6\\#FFFF00\\4\\#FFFFFF\\ Character Select Pack!\n\nThis Pack requires Character Select to use as a Library!
 
 local TEXT_MOD_NAME = "MARIO '96 Pack"
 
@@ -331,13 +331,17 @@ local function on_character_select_load()
 	_G.charSelect.character_add_palette_preset(E_MODEL_PC, PALETTE_PC)
 	_G.charSelect.character_add_palette_preset(E_MODEL_MX, PALETTE_MX)
 
-	_G.charSelect.character_set_category(CT_LUCAS, "MARIO '96")
-	_G.charSelect.character_set_category(CT_FH, "MARIO '96")
-	_G.charSelect.character_set_category(CT_PC, "MARIO '96")
-	_G.charSelect.character_set_category(CT_MX, "MARIO '96")
+	_G.charSelect.character_set_category(CT_LUCAS, TEXT_MOD_NAME)
+	_G.charSelect.character_set_category(CT_FH, TEXT_MOD_NAME)
+	_G.charSelect.character_set_category(CT_PC, TEXT_MOD_NAME)
+	_G.charSelect.character_set_category(CT_MX, TEXT_MOD_NAME)
 
-	-- I'll leave adding credits to someone else, lmao - DM
-	-- _G.charSelect.credit_add(TEXT_MOD_NAME, "5UP34", "Model Creator")
+	_G.charSelect.credit_add(TEXT_MOD_NAME, "5UP34", "Model Creator")
+	_G.charSelect.credit_add(TEXT_MOD_NAME, "Sunset", "Voice Actor")
+	_G.charSelect.credit_add(TEXT_MOD_NAME, "The Hall Monitor", "Voice Actor")
+	_G.charSelect.credit_add(TEXT_MOD_NAME, "Squishy", "Coder")
+	_G.charSelect.credit_add(TEXT_MOD_NAME, "Demi-kun", "Coder")
+	_G.charSelect.credit_add(TEXT_MOD_NAME, "RazzDazzleDoo", "Character Owner")
 
 	CSloaded = true
 end
